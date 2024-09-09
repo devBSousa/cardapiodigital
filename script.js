5be3d7a83401f5b35a00f8c2448a772440b825b4
Dev Bruno Sousa <155699998+buru99@users.noreply.github.com>
2024-09-09 01:51:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,15 @@ const checkoutBtn = document.getElementById("checkout-btn")
 const closeModalBtn = document.getElementById("close-modal-btn")
 const cartCounter = document.getElementById("cart-count")
 const cartObs = document.getElementById("obs")
-const addressInput = document.getElementById("address")
-const addressWarn = document.getElementById("address-warn")
+const nameInput = document.getElementById("name")
+const nameWarn = document.getElementById("name-warn")
+const bairroInput = document.getElementById("bairro")
+const bairroWarn = document.getElementById("bairro-warn")
+const ruaInput = document.getElementById("rua")
+const ruaWarn = document.getElementById("rua-warn")
+const hnumberInput = document.getElementById("housenumber")
+const hnumberWarn = document.getElementById("hnumber-warn")
+const complementoInput = document.getElementById("complemento")
 
 let cart = [];
 
@@ -128,18 +135,38 @@ function removeItemCart(name){
     }
 }
 
-addressInput.addEventListener("input", function(event){
-    let inputValue = event.target.value;
-
-    if(inputValue !== ""){
-        addressInput.classList.remove("border-red-500")
-        addressWarn.classList.add("hidden")
+//funções dos inputs de dados do usuário
+nameInput.addEventListener("input", function(event){
+    let InputValue = event.target.value;
+    
+    if(InputValue !== ""){
+        nameInput.classList.remove("border-red-500")
+        nameWarn.classList.add("hidden")
     }
-
-
 })
 
-   
+bairroInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+    if(inputValue !==""){
+        bairroWarn.classList.add("hidden")
+    }
+})
+
+ruaInput.addEventListener("input", function(event){
+    let InputValue = event.target.value;
+    if(InputValue !==""){
+        ruaWarn.classList.add("hidden")
+    }
+})
+
+hnumberInput.addEventListener("input", function(event){
+    let InputValue = event.target.value;
+    if(InputValue !==""){
+        hnumberWarn.classList.add("hidden")
+    }
+})
+
+//funções botao finalizar pedido
 checkoutBtn.addEventListener("click", function(){
     
     const isOpen = checkopen();
@@ -159,7 +186,6 @@ checkoutBtn.addEventListener("click", function(){
         return;
     }
     
-    //if(cart.length === 0) return;
     if(cart.length === 0){
         Toastify({
             text: "Seu carrinho está vázio!",
@@ -175,11 +201,30 @@ checkoutBtn.addEventListener("click", function(){
           }).showToast();
           return;
     };
-    if(addressInput.value  === ""){
-        addressWarn.classList.remove("hidden")
-        addressInput.classList.add("border-red-500")
+
+    if(nameInput.value === ""){
+        nameWarn.classList.remove("hidden")
+        nameInput.classList.add("border-red-500")
         return;
     }
+
+    if(bairroInput.value === ""){
+        bairroWarn.classList.remove("hidden")
+        return;
+    }
+
+    if(ruaInput.value ===""){
+        ruaWarn.classList.remove("hidden")
+        return;
+    }
+
+    if(hnumberInput.value ===""){
+        hnumberWarn.classList.remove("hidden")
+        return;
+    }
+
+    
+    
     let taxaEntrega = 5
     let total = cart.reduce((acc, item) => acc + item.price * item.quantity + taxaEntrega, 0);
 
@@ -191,7 +236,7 @@ checkoutBtn.addEventListener("click", function(){
         )
     }).join("\n")
     const separator = "-".repeat(50);
-    const message = `*Resumo do Pedido:*\n${cartItems}\n\n*Observação:* ${cartObs.value}\n${separator}\n*Endereço:*\n${addressInput.value}\n${separator}\n*TOTAL + taxa de entrega:* *R$${total.toFixed(2)}*`
+    const message = `*Resumo do Pedido:*\n${cartItems}\n\n*Observação:* ${cartObs.value}\n${separator}\n*Nome:* ${nameInput.value}\n*Rua:* ${ruaInput.value} - ${hnumberInput.value}\n*Bairro* ${bairroInput.value}\n*Complemento:* ${complementoInput.value}\n${separator}\n*TOTAL + taxa de entrega:* *R$${total.toFixed(2)}*`
     const phone = "5588997349933"
 
     window.open(`https://wa.me/${phone}?text=${encodeURIComponent(message)}`, "_blank")
